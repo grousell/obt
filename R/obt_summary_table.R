ed0a793a852d54f896d0a31cc570d7fabf1552da
Greg <grousell@gmail.com>
2022-09-11 12:01:06 -0400

    Update documentation

--- R/obt_summary_table.R
+++ R/obt_summary_table.R
@@ -4,27 +4,25 @@
 #' Given a dataframe or tibble, returns a tibble with counts and percentages
 #' of a group. Can be a single group, or group 2 can be nested within group 1
 #'
-#' @param
-#' df
-#' @param
-#' group_1
-#' @param
-#' group_2
-#' @param
-#' ...
+#' @param df
+#' Dataframe or tibble
+#' @param group_1
+#' First grouping variable
+#' @param group_2
+#' Second grouping variable (optional)
+#'
 #'
 #' @return A tibble with counts and percentages of each group
 #' @export
 #'
 #' @examples
-#' dat <- palmerpenguins::penguins
-#' obt_summary_table (dat, species)
-#' obt_summary_table (dat, species, sex)
-obt_summary_table <- function (df, group_1, group_2, ...){
+#' obt_summary_table (mtcars, gear)
+#' obt_summary_table (mtcars, gear, carb)
+obt_summary_table <- function (df, group_1, group_2){
 
   args <- match.call ()
   exist <- ("group_2" %in% names (args))
-?
+
   tbl <- df |>
     dplyr::group_by({{group_1}}, {{group_2}}) |>
     dplyr::count ()
